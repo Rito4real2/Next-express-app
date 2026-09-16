a8ad70f4be43ad28bb2c5b5794ccdf73d2b7a94a
Rito4real2 <ritolaikpo2@gmail.com>
2026-09-16 12:47:25 +0100

    updated deposit page

--- client/app/users/deposit/page.tsx
+++ client/app/users/deposit/page.tsx
@@ -11,7 +11,7 @@ export default function DepositPage() {
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
-  // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
   const handleDeposit = async (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,7 +20,7 @@ export default function DepositPage() {
     setError(null);
 
     try {
-      const res = await fetch('https://next-express-app-xi.vercel.app/api/transactions/deposit', {
+      const res = await fetch(`${API_BASE_URL}/api/transactions/deposit`, {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',

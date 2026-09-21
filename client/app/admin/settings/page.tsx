49668e4c5c1345636332fb28862d029472f2b167
Rito4real2 <ritolaikpo2@gmail.com>
2026-09-21 19:31:17 +0100

    ADDED: link for admin payment details setting page

--- client/app/admin/settings/page.tsx
+++ client/app/admin/settings/page.tsx
@@ -5,13 +5,15 @@ import Link from 'next/link';
 export default function UserSettingsPage() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <AdminNavbar />
+      <div className="bg-gray-100 rounded-lg border shadow-sm p-6">
+        <AdminNavbar />
+      </div>
       <div className="max-w-4xl mx-auto space-y-6">
         <h1 className="text-2xl font-bold text-gray-800">Admin Settings</h1>
         <ProfileForm />
       </div>
-      <div className="bg-white rounded-lg border shadow-sm p-6 flex flex-col justify-between items-center">
-          <h2 className="text-xl font-bold text-gray-800 mb-4">Amin User Settings</h2>
+      <div className="bg-white rounded-lg border shadow-sm p-6 flex flex-col justify-between items-start">
+          <h2 className="text-xl font-bold text-gray-800 mb-4">Admin User Settings</h2>
           <button>
             <Link href="/users/manage" className="text-blue-600 hover:underline text-sm">
               Manage Users
@@ -21,6 +23,12 @@ export default function UserSettingsPage() {
           <button>
             <Link href="/users/manage/transactions" className="text-blue-600 hover:underline text-sm">
               Manage Transactions
+            </Link>
+          </button>
+
+          <button>
+            <Link href="/admin/settings/payment-details" className="text-blue-600 hover:underline text-sm">
+              Edit Admin Payment Details
             </Link>
           </button>
         </div>

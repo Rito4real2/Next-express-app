ea1e8e46c5f90920621cbf45a3ff7a1d15834e3e
Rito4real2 <ritolaikpo2@gmail.com>
2026-09-22 12:51:33 +0100

    UPDATED: Transaction model to include supported types of crypto

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -23,7 +23,7 @@ const transactionSchema = new mongoose.Schema({
   // Payment method specified by the user
   paymentMethod: {
     type: String,
-    enum: ['BANK_TRANSFER', 'CRYPTO', 'CREDIT_DEBIT_CARD'],
+    enum: ['BANK_TRANSFER', 'CRYPTO', 'CREDIT_DEBIT_CARD', 'BTC', 'ETH', 'USDT', 'USDC'],
     default: 'BANK_TRANSFER'
   },
   // Optional embedded schema for bank transfers
